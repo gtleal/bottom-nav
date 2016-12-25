c6f03e2477e751d2fe01db88f3ea579658f44ee8
Luiz <ramosleal@outlook.com>
2016-12-25 02:33:29 -0300

    Add project with basic implementation

---
+++ BottomNav/app/src/main/java/com/felpudo/bottomnav/MainActivity.java
@@ -0,0 +1,65 @@
+package com.felpudo.bottomnav;
+
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final FragmentManager fragmentManager = getSupportFragmentManager();
+
+        final Fragment songsFragment  = new SongsFragment();
+        final Fragment moviesFragment = new MoviesFragment();
+        final Fragment booksFragment  = new BooksFragment();
+
+        fragmentManager.beginTransaction().replace(R.id.container, songsFragment).commit();
+
+        BottomNavigationView bottomNav = (BottomNavigationView) findViewById(R.id.bottomNav);
+
+        bottomNav.setOnNavigationItemSelectedListener(
+                new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                        FragmentTransaction fragmentTransaction;
+
+                        switch (item.getItemId()) {
+
+                            case R.id.action_songs:
+
+                                fragmentTransaction = fragmentManager.beginTransaction();
+                                fragmentTransaction.replace(R.id.container, songsFragment).commit();
+                                return true;
+
+                            case R.id.action_movies:
+
+                                fragmentTransaction = fragmentManager.beginTransaction();
+                                fragmentTransaction.replace(R.id.container, moviesFragment).commit();
+                                return true;
+
+                            case R.id.action_books:
+
+                                fragmentTransaction = fragmentManager.beginTransaction();
+                                fragmentTransaction.replace(R.id.container, booksFragment).commit();
+                                return true;
+
+                            default:
+                                return true;
+                        }
+                    }
+                }
+        );
+    }
+}
